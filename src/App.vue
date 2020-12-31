9356b91ad89ad1d55d9dd904565cfc4eb32815b3
Steve Calverley <steve.calverley@heffron.com.au>
2020-12-31 11:26:32 +1100

    Install Tailwind CSS

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div>
+    <header class="p-4 bg-gray-800 flex items-baseline space-x-8">
+      <h1 class="text-3xl text-gray-50">Mistify</h1>
+      <nav class="flex space-x-2 text-gray-50">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </nav>
+    </header>
+    <main class="container mx-auto mt-4 ">
+      <router-view />
+    </main>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
