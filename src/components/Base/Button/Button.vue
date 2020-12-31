222580e9da55837656210986b5a85815293aac21
Steve Calverley <steve.calverley@heffron.com.au>
2020-12-31 12:06:55 +1100

    Add Focus state

--- src/components/Base/Button/Button.vue
+++ src/components/Base/Button/Button.vue
@@ -1,5 +1,8 @@
 <template>
-  <button class="px-2 py-1 bg-blue-600 rounded text-white" @click="handleClick">
+  <button
+    class="px-2 py-1 bg-blue-600 rounded text-white focus:outline-none focus:border-transparent focus:ring focus:ring-blue-400 focus:ring-opacity-75"
+    @click="handleClick"
+  >
     <slot />
   </button>
 </template>
